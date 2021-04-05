afdefaa8097f1fabc6c3f2b3142e0db947033a3c
nanquanyuhao <nanquanyuhao@foxmail.com>
2021-04-05 12:27:23 +0800

    gateway基础示例

---
+++ cloud-gateway-gateway9527/src/main/java/net/nanquanyuhao/springcloud/GateWayMain9572.java
@@ -0,0 +1,14 @@
+package net.nanquanyuhao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9572 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9572.class, args);
+    }
+}
