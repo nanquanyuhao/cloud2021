5511a6ff05786be2013ec0384e6d07c03b70f6e3
nanquanyuhao <nanquanyuhao@foxmail.com>
2021-04-05 09:21:51 +0800

    引入hystrix

---
+++ cloud-provider-hystrix-payment8001/src/main/java/net/nanquanyuhao/springcloud/service/PaymentService.java
@@ -0,0 +1,32 @@
+package net.nanquanyuhao.springcloud.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class PaymentService {
+
+    /**
+     * 正常访问，肯定 OK
+     *
+     * @param id
+     * @return
+     */
+    public String paymentInfo_OK(Integer id) {
+        return "线程池： " + Thread.currentThread().getName() + " paymentInfo_OK,id: " + id + "\t" + "哈哈";
+    }
+
+    public String paymentInfo_TimeOut(Integer id) {
+
+        int timeNumber = 3;
+        try {
+            TimeUnit.SECONDS.sleep(timeNumber);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "线程池： " + Thread.currentThread().getName() + " paymentInfo_TimeOut,id: " + id +
+                "\t" + "哈哈" + " 耗时（秒）" + timeNumber;
+    }
+}
