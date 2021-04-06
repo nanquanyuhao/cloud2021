e86ce040f153f5c20cab43a25912d2529788937b
nanquanyuhao <nanquanyuhao@foxmail.com>
2021-04-06 20:53:08 +0800

    nacos配置中心基本示例完成

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/net/nanquanyuhao/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package net.nanquanyuhao.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //支持配置自动刷新
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping(value = "/config/info")
+    public String getPayment() {
+        return configInfo;
+    }
+}
